2acc844d2d93316bdee388ea6b26e2ab557ad4df
brueni <info@michael-bruenisholz.ch>
2017-03-19 12:59:04 +0000

    First working version

--- panel.sh
+++ panel.sh
@@ -43,37 +43,71 @@ btn04="/sys/class/gpio/gpio64/value"
 btn03="/sys/class/gpio/gpio65/value"
 btn08="/sys/class/gpio/gpio67/value"
 
+baseaction="http://homecontrol-backend.home/homecontrol-backend/"
+
+btn01action="light-mystrom.php?switch=mystrom4.home&action=toggle" #Standlampe
+btn02action="light-mystrom.php?switch=mystrom3.home&action=toggle" #Ecklampe
+btn03action="light-mystrom.php?switch=mystrom2.home&action=toggle" #Kugellampe
+btn04action="set_nrdylight.php?mode=toggle" #Ambilight
+btn05action=""
+btn06action=""
+btn07action=""
+btn08action=""
+btn09action=""
+btn10action="all_off.php" #All off
+
+
 for (( ; ; ))
 do
     if [ $(cat $btn01) = "0" ]; then
         echo "btn01 pressed"
+	curl -s $baseaction$btn01action
+	sleep 1
     fi
     if [ $(cat $btn02) = "0" ]; then
         echo "btn02 pressed"
+	curl -s $baseaction$btn02action
+        sleep 1
     fi
     if [ $(cat $btn03) = "0" ]; then
         echo "btn03 pressed"
+	curl -s $baseaction$btn03action
+        sleep 1
     fi
     if [ $(cat $btn04) = "0" ]; then
         echo "btn04 pressed"
+	curl -s $baseaction$btn04action
+        sleep 1
     fi
     if [ $(cat $btn05) = "0" ]; then
         echo "btn05 pressed"
+	curl -s $baseaction$btn05action
+        sleep 1
     fi
     if [ $(cat $btn06) = "0" ]; then
         echo "btn06 pressed"
+	curl -s $baseaction$btn06action
+        sleep 1
     fi
     if [ $(cat $btn07) = "0" ]; then
         echo "btn07 pressed"
+	curl -s $baseaction$btn07action
+        sleep 1
     fi
     if [ $(cat $btn08) = "0" ]; then
         echo "btn08 pressed"
+	curl -s $baseaction$btn08action
+        sleep 1
     fi
     if [ $(cat $btn09) = "0" ]; then
         echo "btn09 pressed"
+	curl -s $baseaction$btn09action
+        sleep 1
     fi
     if [ $(cat $btn10) = "0" ]; then
         echo "btn10 pressed"
+	curl -s $baseaction$btn10action
+        sleep 1
     fi
     sleep 1
 done
